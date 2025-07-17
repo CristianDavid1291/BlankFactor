a136c145c566ee753997800c0c085ccc761b5cbb
Cristian <cristiandavid1291@gmail.com>
2025-07-17 15:30:10 -0500

    refactoring

--- features/locators/__init__.py
+++ features/locators/__init__.py
@@ -6,11 +6,9 @@ Centralized locator management for BlankFactor test framework
 from .home_page_locators import HomePageLocators
 from .industries_page_locators import IndustriesPageLocators
 from .retirement_wealth_page_locators import RetirementWealthPageLocators
-from .contact_page_locators import ContactPageLocators
 
 __all__ = [
     'HomePageLocators', 
     'IndustriesPageLocators',
-    'RetirementWealthPageLocators',
-    'ContactPageLocators'
+    'RetirementWealthPageLocators'
 ]
